8b688173bdaff54c3e0c5f4f28223a0409821ad9
Tapan Padhy <tprintu@gmail.com>
2024-03-29 16:42:39 +0530

    jst add a printing statement

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Test2 {
 	public static void main(String[] args) 
     {
+		System.out.println("try to modified");
         int r;
         double pi = 3.14, area;
         Scanner s = new Scanner(System.in);
@@ -10,6 +11,7 @@ public class Test2 {
         r = s.nextInt();
         area = pi * r * r;
         System.out.println("Area of circle:"+area);
+        
     }        
 
 }
