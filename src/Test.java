1a03fbf67fcc8d07e8aec3c1b7211d8d74c4e9ab
Tapan Padhy <tprintu@gmail.com>
2024-03-29 15:48:23 +0530

    Try to add

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "yes") {
+			System.out.println("lets it......");
+		} else {
+			System.out.println("do your work");
+		}
+	}
+}
